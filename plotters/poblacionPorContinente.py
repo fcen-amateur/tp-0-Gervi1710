26962947eb514599897197eeac587e61508b84de
Gervi1710 <164246301+Gervi1710@users.noreply.github.com>
2024-04-23 14:27:44 -0300

    Update poblacionPorContinente.py

--- plotters/poblacionPorContinente.py
+++ plotters/poblacionPorContinente.py
@@ -1,1 +1,19 @@
+import seaborn.objects as so
+from gapminder import gapminder
 
+PopContinent=gapminder.groupby(["continent","year"])["pop"].sum()
+PopContinentDF=pd.DataFrame(PopContinent)
+
+def plot():
+    figura = (
+                so.Plot(data=PopContinentDF, x="year", y="pop", color="continent")
+                .add(so.Line())
+                .label(title="Evolución de la población por continente", x="Año", y="Población (escala logarítmica)")
+                .scale(y="log")
+        )
+    )
+    return dict(
+        descripcion="Un sofisticado gráfico con el número de países en cada continente",
+        autor="La cátedra",
+        figura=figura,
+    )
